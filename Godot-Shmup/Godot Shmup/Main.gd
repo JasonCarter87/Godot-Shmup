cf2efbdc89fe3baaba72e623cb4df4e597510476
JasonCarter87 <35275412+JasonCarter87@users.noreply.github.com>
2019-01-16 18:39:39 -0500

    Add files via upload

---
+++ Godot-Shmup/Godot Shmup/Main.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	$"ScoreLabel".text = "Score: " + str(global.score)
+	
+	if Input.is_action_just_pressed("restart_game") and global.isGameOver:
+		go.restart_scene()
