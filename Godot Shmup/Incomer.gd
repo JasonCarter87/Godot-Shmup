745fb6fcbf5806d93b9ae3fa7b6369f08ee9e0e5
JasonCarter87 <35275412+JasonCarter87@users.noreply.github.com>
2019-01-23 18:13:42 -0500

    Completed Second Milestone, Buddy Ship

---
+++ Godot Shmup/Incomer.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+var health = 3
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	self.position.y += 2
+	pass
+func area_entered(otherArea):
+	self.health -= 1
+	if self.health < 1:
+		go.destroy(self)
+	 	#print("Yikes thats a collision")
+	global.score += 10
+	print(global.score)
+	
+	if otherArea.get_parent().name == "Player":
+		print("Game Over")
+		global.isGameOver = true
+	
